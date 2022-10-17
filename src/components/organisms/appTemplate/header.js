6d0da34708466f747daaef8b05cf5a5286d55f48
ArtitayaKhaudsri <artitaya.kha@dome.tu.ac.th>
2022-10-17 21:02:51 +0700

    Header bar may be added

--- src/components/organisms/appTemplate/header.js
+++ src/components/organisms/appTemplate/header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Text, View, HStack, Icon} from "native-base";
+import icons from "../../../utils/icons";
 
 class Header extends React.Component {
   constructor(props) {
@@ -6,8 +8,26 @@ class Header extends React.Component {
   }
 
   render() {
+
     return (
-      <></>
+        <>
+
+          <View style={{paddingLeft:"3%", paddingRight:"3%"}}>
+            <View>
+               <Text style={{fontSize:15}}>
+                 {"ตำแหน่งปัจจุบัน"}
+               </Text>
+               <HStack>
+                 <Text numberOfLines={1} style={{width:"90%", fontSize:20}}>{"ABCDEFGHIJKLMNOPQRSTUVWXYABCDEFGHIJKLMNOPQRSTUVWXY"}</Text>
+               </HStack>
+               <View style={{position:"absolute", paddingTop:"3%", paddingLeft:"87%", display:"flex", flexDirection: "row"}}>
+                 <Icon as={icons.Octicons} name="pencil" size="30px" color="black"/>
+                 <Icon as={icons.AntDesign} name="message1" size="30px" color="black"/>
+               </View>
+            </View>
+          </View>
+
+        </>
     );
   }
 }
