fdd2cac23179ce913cd039ea819ad181efe01247
USER <artitaya.kha@dome.tu.ac.th>
2022-10-20 19:02:52 +0700

    Header bar

--- src/components/organisms/appTemplate/header.js
+++ src/components/organisms/appTemplate/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Text, View, HStack, Icon} from "native-base";
+import {Text, View, HStack, Icon, Image, VStack, Box} from "native-base";
 import icons from "../../../utils/icons";
+import bg from "../../../assets/image/bgHeader.png"
 
 class Header extends React.Component {
   constructor(props) {
@@ -11,22 +12,25 @@ class Header extends React.Component {
 
     return (
         <>
-
-          <View style={{paddingLeft:"3%", paddingRight:"3%"}}>
-            <View>
-               <Text style={{fontSize:15}}>
-                 {"ตำแหน่งปัจจุบัน"}
-               </Text>
-               <HStack>
-                 <Text numberOfLines={1} style={{width:"90%", fontSize:20}}>{"ABCDEFGHIJKLMNOPQRSTUVWXYABCDEFGHIJKLMNOPQRSTUVWXY"}</Text>
-               </HStack>
-               <View style={{position:"absolute", paddingTop:"3%", paddingLeft:"87%", display:"flex", flexDirection: "row"}}>
-                 <Icon as={icons.Octicons} name="pencil" size="30px" color="black"/>
-                 <Icon as={icons.AntDesign} name="message1" size="30px" color="black"/>
-               </View>
-            </View>
-          </View>
-
+        <View style={{paddingBottom:"4%"}}>
+          <Image style={{position:"absolute", width:"100%"}} source={bg} alt="bgHeader"/>
+        </View>
+        <View style={{paddingTop:"4%", paddingLeft:"3%", paddingRight:"3%"}}>
+          <HStack style={{width:"70%"}}>
+            <VStack>
+              <Text style={{fontSize:15, color:"white"}}>{"ตำแหน่งปัจจุบัน"}</Text>
+              <Text numberOfLines={1} style={{fontSize:20, paddingTop:"1%", color:"white"}}>{"ABCDEFGHIJKLMNOPQRSTUVWXYABCDEFGHIJKLMNOPQRSTUVWXY"}</Text>
+            </VStack>
+            <HStack p="9" style={{paddingTop:"3%"}}>
+              <Box pr="4">
+                <Icon as={icons.Octicons} name="pencil" size="32px" color="white" solid />
+              </Box>
+              <Box>
+                <Icon as={icons.AntDesign} name="message1" size="32px" color="white" solid />
+              </Box>
+            </HStack>
+          </HStack>
+        </View>
         </>
     );
   }
