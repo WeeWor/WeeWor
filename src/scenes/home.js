8cb6e48a7a6dba05f83a9908a59b375c5ea7388e
JeryT <kullataya.n@gmail.com>
2022-10-28 19:37:08 +0700

    home screen

--- src/scenes/home.js
+++ src/scenes/home.js
@@ -1,23 +1,101 @@
 import React from "react";
-import { Icon, Text } from "native-base";
+import {HStack, Icon, Image, Text, View, ScrollView, VStack} from "native-base";
 import AppTemplate from "../components/templates/app";
+import { ImageSlider } from "react-native-image-slider-banner";
 import Icons from "../utils/icons";
+import center from "native-base/src/theme/components/center";
+
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
+      this.state = {
+
+      };
+
   }
 
   render() {
     return (
       <AppTemplate {...this.props}>
-        <Text fontSize="lg">lg</Text>
-        <Icon as={Icons.FontAwesome5} name="user-alt" size="md" color="black"/>
-        <Text fontSize="8xl">lg</Text>
-        <Text fontSize="8xl">lg</Text>
-        <Text fontSize="8xl">lg</Text>
-        <Text fontSize="8xl">lg</Text>
-        <Text fontSize="8xl">lg</Text>
+          <ScrollView>
+              <View paddingLeft={3} paddingRight={3}>
+                  <ImageSlider
+                      data={[
+                          {img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5a5uCP-n4teeW2SApcIqUrcQApev8ZVCJkA&usqp=CAU'},
+                          {img: 'https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg'},
+                          {img: 'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg'}
+                      ]}
+                      autoPlay={false}
+                      onItemChanged={(item) => console.log("item", item)}
+                      closeIconColor="#fff"
+                      preview={false}
+                  />
+              </View>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <Text bold fontSize="2xl" marginLeft={3} marginBottom={3}>หน่วยงานที่ใกล้คุณ</Text>
+                  <Text marginTop={2} fontSize="lg">เพิ่มเติม ></Text>
+              </View>
+              <ScrollView horizontal={true}>
+                  <HStack flex={3}>
+                      <VStack alignItems={'center'} width={150}>
+                      <Image borderRadius={10} source={{
+                          uri: "https://wallpaperaccess.com/full/317501.jpg"
+                      }} alt="Alternate Text" size="xl" />
+                          <Text>SoB</Text>
+                      </VStack>
+                      <VStack alignItems={'center'} width={150}>
+                      <Image borderRadius={10} source={{
+                          uri: "https://wallpaperaccess.com/full/317501.jpg"
+                      }} alt="Alternate Text" size="xl" />
+                      <Text>SoB</Text>
+                      </VStack>
+                      <VStack alignItems={'center'} width={150}>
+                          <Image borderRadius={10} source={{
+                              uri: "https://wallpaperaccess.com/full/317501.jpg"
+                          }} alt="Alternate Text" size="xl" />
+                          <Text>SoB</Text>
+                      </VStack>
+                      <VStack alignItems={'center'} width={150}>
+                          <Image borderRadius={10} source={{
+                              uri: "https://wallpaperaccess.com/full/317501.jpg"
+                          }} alt="Alternate Text" size="xl" />
+                          <Text>SoB</Text>
+                      </VStack>
+                  </HStack>
+              </ScrollView>
+              <Text bold fontSize="2xl" marginLeft={3} marginBottom={3}>วิธีการรับมืออุบัติเหตุต่าง ๆ</Text>
+              <VStack>
+                  <HStack>
+                      <VStack width={'50%'} alignItems={'center'}>
+                          <Image borderRadius={10} source={{
+                              uri: "https://wallpaperaccess.com/full/317501.jpg"
+                          }} alt="Alternate Text" width={180} height={180} />
+                          <Text>SoB</Text>
+                      </VStack>
+                      <VStack width={'50%'} alignItems={'center'}>
+                          <Image borderRadius={10} source={{
+                              uri: "https://wallpaperaccess.com/full/317501.jpg"
+                          }} alt="Alternate Text" width={180} height={180} />
+                          <Text>SoB</Text>
+                      </VStack>
+                  </HStack>
+                  <HStack>
+                      <VStack width={'50%'} alignItems={'center'}>
+                          <Image borderRadius={10} source={{
+                              uri: "https://wallpaperaccess.com/full/317501.jpg"
+                          }} alt="Alternate Text" width={180} height={180} />
+                          <Text>SoB</Text>
+                      </VStack>
+                      <VStack width={'50%'} alignItems={'center'}>
+                          <Image borderRadius={10} source={{
+                              uri: "https://wallpaperaccess.com/full/317501.jpg"
+                          }} alt="Alternate Text" width={180} height={180} />
+                          <Text>SoB</Text>
+                      </VStack>
+                  </HStack>
+              </VStack>
+          </ScrollView>
       </AppTemplate>
     );
   }
