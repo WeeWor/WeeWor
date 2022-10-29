de49180983274f1ffbd4f6783f5b5a68831456c6
MrTheBank <thebankzana@yahoo.com>
2022-10-29 23:16:11 +0700

    onpress event of unit detail box and guide box

--- src/scenes/home.js
+++ src/scenes/home.js
@@ -1,8 +1,7 @@
 import React from "react";
-import {HStack, Icon, Image, Text, View, ScrollView, VStack} from "native-base";
+import {HStack, Text, View, ScrollView, VStack} from "native-base";
 import AppTemplate from "../components/templates/app";
 import { ImageSlider } from "react-native-image-slider-banner";
-import center from "native-base/src/theme/components/center";
 import UnitBox from "../components/molecules/unit-box";
 import GuideBox from "../components/molecules/guide-box";
 
@@ -17,6 +16,8 @@ class Home extends React.Component {
   }
 
   render() {
+    const { navigation } = this.props;
+
     return (
       <AppTemplate {...this.props}>
           <ScrollView>
@@ -38,7 +39,7 @@ class Home extends React.Component {
               </View>
               <ScrollView horizontal={true}>
                   <HStack flex={3}>
-                      <UnitBox img="https://wallpaperaccess.com/full/317501.jpg" text="SoB"/>
+                      <UnitBox img="https://wallpaperaccess.com/full/317501.jpg" text="SoB" onPress={() => navigation.push('UnitDetail')}/>
                       <UnitBox img="https://wallpaperaccess.com/full/317501.jpg" text="SoB"/>
                       <UnitBox img="https://wallpaperaccess.com/full/317501.jpg" text="SoB"/>
                       <UnitBox img="https://wallpaperaccess.com/full/317501.jpg" text="SoB"/>
@@ -47,7 +48,7 @@ class Home extends React.Component {
               <Text bold fontSize="2xl" marginLeft={3} marginBottom={3}>วิธีการรับมืออุบัติเหตุต่าง ๆ</Text>
               <VStack>
                   <HStack space={4} px={2}>
-                    <GuideBox img="https://wallpaperaccess.com/full/317501.jpg" text="SoB"/>
+                    <GuideBox img="https://wallpaperaccess.com/full/317501.jpg" text="SoB" onPress={() => navigation.push('Guide')}/>
                     <GuideBox img="https://wallpaperaccess.com/full/317501.jpg" text="SoB"/>
                   </HStack>
                   <HStack space={4} px={2}>
