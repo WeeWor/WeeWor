dc6f9e3f89e119c762f28da4b09bcc1a23db23ff
MrTheBank <thebankzana@yahoo.com>
2022-10-29 23:14:36 +0700

    add template screen (guide, unit detail, map)

---
+++ src/scenes/map.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text } from "native-base";
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Text fontSize="lg">map</Text>
+    );
+  }
+}
+
+export default Map;
