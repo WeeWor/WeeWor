828c670a531e7a73685d2da7461b6059d90bb8dc
kim-pukkanun <pukkanun.som@dome.tu.ac.th>
2022-11-01 23:43:48 +0700

    Create map pin and confirm button

--- src/scenes/map.js
+++ src/scenes/map.js
@@ -1,39 +1,60 @@
 import React from "react";
-import { Text } from "native-base";
+import {Button, Container, Text} from "native-base";
 import { StyleSheet} from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, {Marker, PROVIDER_GOOGLE} from "react-native-maps";
 
 class Map extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        position: {
+            latitude: 37.78825,
+            longitude: -122.4324,
+        }
+    }
+  }
+
+  confirmation() {
+      console.log(this.state.position.latitude);
+      console.log(this.state.position.longitude);
   }
 
   render() {
     return (
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}/>
+        <Container style={{height:"100%", width: 400}}>
+            <MapView onPress={(e) => this.setState({ position: e.nativeEvent.coordinate })}
+                 provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                 style={styles.map}
+                 region={{
+                     latitude: 37.78825,
+                     longitude: -122.4324,
+                     latitudeDelta: 0.015,
+                     longitudeDelta: 0.0121,
+                 }}
+            >
+                <Marker
+                    coordinate={this.state.position}
+                />
+            </MapView>
+            <Button style={styles.button} onPress={() => this.confirmation()}>
+                <Text bold fontSize={"2xl"} style={{color: "white"}}>ยืนยัน</Text>
+            </Button>
+        </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+    map: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+        width: 400,
+    },
+    button: {
+        position: "absolute",
+        bottom: 5,
+        left: 5,
+        width: 380
+    }
 });
 
 export default Map;
