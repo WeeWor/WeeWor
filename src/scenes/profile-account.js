81fa4bfd8b91cc8ac7ec7a72c8e2ede1fb8bfefa
MrTheBank <thebankzana@yahoo.com>
2022-11-03 19:03:54 +0700

    profile edit form, storage

--- src/scenes/profile-account.js
+++ src/scenes/profile-account.js
@@ -1,5 +1,17 @@
 import React from "react";
-import { Button, Center, ChevronLeftIcon, FormControl, HStack, Image, Input, Modal, Text, VStack } from "native-base";
+import {
+  Button,
+  Center,
+  ChevronLeftIcon,
+  Divider,
+  FormControl,
+  HStack,
+  Image,
+  Input,
+  Modal,
+  Text,
+  VStack,
+} from "native-base";
 import AppTemplate from "../components/templates/app";
 
 class ProfileAccount extends React.Component {
@@ -8,7 +20,10 @@ class ProfileAccount extends React.Component {
     this.state = {
       modal: false,
       form: {
-        password: ''
+        email: '',
+        old_password: '',
+        new_password: '',
+        confirm_password: ''
       }
     };
   }
@@ -30,9 +45,9 @@ class ProfileAccount extends React.Component {
                   <Text fontSize="2xl" paddingTop={"10px"} bold>USERNAME</Text>
               </Center>
           </HStack>
-          <Text fontSize="2xl" paddingLeft={"20px"} paddingTop={"10px"} bold>ID : 57439853823 </Text>
+          <Text fontSize="2xl" paddingLeft={"20px"} paddingTop={"10px"} bold>Username : 57439853823 </Text>
           <VStack space={"248px"}>
-              <Text fontSize="2xl" paddingLeft={"20px"} paddingTop={"10px"} bold>Password : ****** </Text>
+              <Text fontSize="2xl" paddingLeft={"20px"} paddingTop={"10px"} bold>E-Mail : myemail@mail.com </Text>
               <Text fontSize="lg" paddingRight={"20px"} bold textAlign="right" color={"red.500"} onPress={() => this.setState({modal: true})}>แก้ไข</Text>
           </VStack>
 
@@ -42,8 +57,21 @@ class ProfileAccount extends React.Component {
             <Modal.Header><Text>แก้ไขบัญชี</Text></Modal.Header>
             <Modal.Body>
               <FormControl>
-                <FormControl.Label><Text>input jaa</Text></FormControl.Label>
-                <Input value={this.state.form.password} onChangeText={(text => this.setState({form: {...this.state.form, password: text}}))} bgColor="#f8f8f8"/>
+                <FormControl.Label><Text>E-Mail</Text></FormControl.Label>
+                <Input value={this.state.form.email} onChangeText={(text => this.setState({form: {...this.state.form, email: text}}))} bgColor="#f8f8f8"/>
+              </FormControl>
+              <Divider mt={4}/>
+              <FormControl>
+                <FormControl.Label><Text>Old Password</Text></FormControl.Label>
+                <Input value={this.state.form.old_password} onChangeText={(text => this.setState({form: {...this.state.form, old_password: text}}))} bgColor="#f8f8f8"/>
+              </FormControl>
+              <FormControl>
+                <FormControl.Label><Text>New Password</Text></FormControl.Label>
+                <Input value={this.state.form.new_password} onChangeText={(text => this.setState({form: {...this.state.form, new_password: text}}))} bgColor="#f8f8f8"/>
+              </FormControl>
+              <FormControl>
+                <FormControl.Label><Text>Confirm New Password</Text></FormControl.Label>
+                <Input value={this.state.form.confirm_password} onChangeText={(text => this.setState({form: {...this.state.form, confirm_password: text}}))} bgColor="#f8f8f8"/>
               </FormControl>
             </Modal.Body>
             <Modal.Footer>
