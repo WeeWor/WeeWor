c2f6ad34ea77ff44eba15ca2ca35241298caafe8
MrTheBank <thebankzana@yahoo.com>
2022-11-05 20:43:53 +0700

    fix guide and unit detail

--- src/scenes/guide.js
+++ src/scenes/guide.js
@@ -1,9 +1,6 @@
 import React from "react";
-import {Button, Image, ScrollView, Text, View, VStack} from "native-base";
+import {Button, Image, ScrollView, Text, View} from "native-base";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
-import Feather from "react-native-vector-icons/Feather";
-import AntDesign from "react-native-vector-icons/AntDesign";
-import AppTemplate from "../components/templates/app";
 
 class Guide extends React.Component {
   constructor(props) {
@@ -12,27 +9,28 @@ class Guide extends React.Component {
 
   render() {
     return (
-        <AppTemplate {...this.props}>
-          <ScrollView style={{backgroundColor:'white'}}>
-            <Image
-                style={{width:'100%', height:250}}
-                source={{uri: 'https://img.kapook.com/u/2016/kantana/12health/ppa4_1.jpg'}}
-            />
-            <View width={'100%'} style={{alignSelf: 'flex-end',
-              position: 'absolute',
-            }}>
-              <Button width={35} height={35} backgroundColor={'white'} opacity={0.7} borderRadius={100}><FontAwesome5 name={'chevron-left'} color={'black'} size={15}/></Button>
-            </View>
-            <View>
-              <Text bold fontSize="4xl" margin={3}>
-                การช่วยเหลือคนจมน้ำ
-              </Text>
-              <Text fontSize="2xl" margin={3}>
-                ผู้ช่วยเหลือยืดแขนและไหล่เหยียดตรง แล้วปล่อยน้ำหนักตัวผ่านจากไหล่ไปที่ลำแขนสองข้าง และลงไปสู่กระดูกหน้าอกในแนวตั้งฉากกับลำตัว กดลลงลึกประมาณ 2 นิ้ว หรือราว ๆ 5 เซนติเมตรของความหนาหน้าอก ให้กดลงไปในแนวดิ่งและอย่ากระแทก ให้ทำสลับกับการเป่าปาก ใช้เทคนิคการเป่าปาก 2 ครั้ง/การกดหน้าอก 30 ครั้ง
-              </Text>
-            </View>
-          </ScrollView>
-        </AppTemplate>
+      <ScrollView style={{backgroundColor:'white'}}>
+        <Image
+            style={{width:'100%', height:250}}
+            source={{uri: 'https://img.kapook.com/u/2016/kantana/12health/ppa4_1.jpg'}}
+            alt=""
+        />
+        <View width={'100%'} style={{alignSelf: 'flex-end',
+          position: 'absolute',
+          left: 8,
+          top: 8
+        }}>
+          <Button width={35} height={35} backgroundColor={'white'} opacity={0.7} borderRadius={100} onPress={() => this.props.navigation.goBack()}><FontAwesome5 name={'chevron-left'} color={'black'} size={15}/></Button>
+        </View>
+        <View>
+          <Text bold fontSize="4xl" margin={3}>
+            การช่วยเหลือคนจมน้ำ
+          </Text>
+          <Text fontSize="2xl" margin={3}>
+            ผู้ช่วยเหลือยืดแขนและไหล่เหยียดตรง แล้วปล่อยน้ำหนักตัวผ่านจากไหล่ไปที่ลำแขนสองข้าง และลงไปสู่กระดูกหน้าอกในแนวตั้งฉากกับลำตัว กดลลงลึกประมาณ 2 นิ้ว หรือราว ๆ 5 เซนติเมตรของความหนาหน้าอก ให้กดลงไปในแนวดิ่งและอย่ากระแทก ให้ทำสลับกับการเป่าปาก ใช้เทคนิคการเป่าปาก 2 ครั้ง/การกดหน้าอก 30 ครั้ง
+          </Text>
+        </View>
+      </ScrollView>
     );
   }
 }
--- src/scenes/unit-detail.js
+++ src/scenes/unit-detail.js
@@ -1,5 +1,4 @@
 import React from "react";
-import AppTemplate from "../components/templates/app";
 import {Text, View, Image, ScrollView, Button, VStack} from "native-base";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import Feather from "react-native-vector-icons/Feather";
@@ -12,43 +11,44 @@ class UnitDetail extends React.Component {
 
   render() {
     return (
-        <AppTemplate {...this.props}>
-          <ScrollView style={{backgroundColor:'white'}}>
-            <Image
-                style={{width:'100%', height:250}}
-                source={{uri: 'https://kasemradvientiane.com/images/kasemrad-vientiane/kasemrad-international-hospital-vientiane/kasemrad-international-hospital-vientiane-02.jpg'}}
-            />
-            <View width={'100%'} style={{alignSelf: 'flex-end',
-              position: 'absolute',
-            }}>
-              <Button width={35} height={35} backgroundColor={'white'} opacity={0.7} borderRadius={100}><FontAwesome5 name={'chevron-left'} color={'black'} size={15}/></Button>
-            </View>
-            <View>
-              <Text bold fontSize="4xl" margin={3}>
-                Kasemrad International Hospital
-              </Text>
-              <Text fontSize="2xl" margin={3}>
-                No.999, 450 years Road,
-                Donnokkhoum Village, Sisattanak District,
-                Vientiane Capital, Lao PDR
-              </Text>
-              <VStack marginTop={5} marginBottom={5} space={4} px={2} alignItems={'center'} width={'100%'}>
-                <Button width={'90%'} backgroundColor={"red.400"} borderRadius={40}>
-                  <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <Text  style={{color:'black', fontSize:26,lineHeight: 35 }} paddingRight={2}>โทร</Text>
-                    <Feather name={'phone-call'} color={'black'} size={28}/>
-                  </View>
-                </Button>
-                <Button width={'90%'} backgroundColor={"red.400"} borderRadius={40}>
-                  <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <Text  style={{color:'black', fontSize:26,lineHeight: 35 }} paddingRight={2}>ข้อความ</Text>
-                    <AntDesign name={'message1'} color={'black'} size={28}/>
-                  </View>
-                </Button>
-              </VStack>
-            </View>
-          </ScrollView>
-        </AppTemplate>
+      <ScrollView style={{backgroundColor:'white'}}>
+        <Image
+            style={{width:'100%', height:250}}
+            source={{uri: 'https://kasemradvientiane.com/images/kasemrad-vientiane/kasemrad-international-hospital-vientiane/kasemrad-international-hospital-vientiane-02.jpg'}}
+            alt=""
+        />
+        <View width={'100%'} style={{alignSelf: 'flex-end',
+          position: 'absolute',
+          left: 8,
+          top: 8
+        }}>
+          <Button width={35} height={35} backgroundColor={'white'} opacity={0.7} borderRadius={100} onPress={() => this.props.navigation.goBack()}><FontAwesome5 name={'chevron-left'} color={'black'} size={15}/></Button>
+        </View>
+        <View>
+          <Text bold fontSize="4xl" margin={3}>
+            Kasemrad International Hospital
+          </Text>
+          <Text fontSize="2xl" margin={3}>
+            No.999, 450 years Road,
+            Donnokkhoum Village, Sisattanak District,
+            Vientiane Capital, Lao PDR
+          </Text>
+          <VStack marginTop={5} marginBottom={5} space={4} px={2} alignItems={'center'} width={'100%'}>
+            <Button width={'90%'} backgroundColor={"red.400"} borderRadius={40}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Text  style={{color:'black', fontSize:26,lineHeight: 35 }} paddingRight={2}>โทร</Text>
+                <Feather name={'phone-call'} color={'black'} size={28}/>
+              </View>
+            </Button>
+            <Button width={'90%'} backgroundColor={"red.400"} borderRadius={40}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Text  style={{color:'black', fontSize:26,lineHeight: 35 }} paddingRight={2}>ข้อความ</Text>
+                <AntDesign name={'message1'} color={'black'} size={28}/>
+              </View>
+            </Button>
+          </VStack>
+        </View>
+      </ScrollView>
     );
   }
 }
